a6b0d241d8e21b2af987397967d289ce56382d48
Sebastian Röder <no-spam-github@sebastianroeder.de>
2014-09-14 17:35:09 +0200

    Connect to the blog feed URL in an AsyncTask

--- app/src/main/java/de/sebastianroeder/blogreader/MainListActivity.java
+++ app/src/main/java/de/sebastianroeder/blogreader/MainListActivity.java
@@ -1,9 +1,19 @@
 package de.sebastianroeder.blogreader;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainListActivity extends ListActivity {
@@ -19,8 +29,18 @@ public class MainListActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
+        if (isNetworkAvailable()) {
+            try {
+                URL feedURL = new URL(FEED_URL + NUMBER_OF_POSTS);
+                GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+                getBlogPostsTask.execute(feedURL);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "MalformedURLException caught: ", e);
+            }
+        } else {
+            Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
+        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,5 +59,28 @@ public class MainListActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    private class GetBlogPostsTask extends AsyncTask<URL, Void, Void> {
+
+        @Override
+        protected Void doInBackground(URL... feedURLs) {
+            try {
+                HttpURLConnection connection = (HttpURLConnection) feedURLs[0].openConnection();
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                Log.i(TAG, "HTTP Response Code: " + responseCode);
+            } catch (java.io.IOException e) {
+                Log.e(TAG, "IOException caught: ", e);
+            }
+            return null;
+        }
     }
 }
